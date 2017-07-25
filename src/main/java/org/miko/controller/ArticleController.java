fb7ee7a477e293099a93f845183c1adbe2f23f67
m1koo <136057505@qq.com>
2017-07-25 15:30:52 +0800

    diary

--- src/main/java/org/miko/controller/ArticleController.java
+++ src/main/java/org/miko/controller/ArticleController.java
@@ -1,10 +1,14 @@
 package org.miko.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -15,22 +19,13 @@ public class ArticleController {
 
     @RequestMapping(value = "/upload")
     @ResponseBody
-    public String upload(@RequestParam("a") String json) throws IOException, URISyntaxException {
+    public String upload(@RequestParam MultipartFile[] myfiles, HttpServletRequest request) throws IOException {
 
-        URL is = this.getClass().getClassLoader().getResource("a.xml");
-        return txt2String(is);
+
+        System.out.println(myfiles.length);
+        System.out.println(request.getParameter("contentJson"));
+
+        return "success";
     }
 
-    public String txt2String(URL is) throws IOException, URISyntaxException {
-        if(is == null){
-            System.out.println("fadsfadfdafadsfad");
-        }
-        StringBuilder result = new StringBuilder();
-        String line = null;
-        BufferedReader reader = new BufferedReader(new FileReader(new File(is.toURI())));
-        while ((line = reader.readLine())!=null) {
-            result.append(line);
-        }
-        return result.toString();
-    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,26 +3,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+    <title>用户上传图片页面</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>北大</title>
 </head>
-<style type="text/css">
-    body {
-        margin: 0px;
-        padding: 0px;
-    }
-
-    .d1 {
-        position: absolute;
-        left: 0px;
-        top: 0px;
-        width: 100%;
-        height: 100%;
-        z-index: -1;
-    }
-</style>
 <body>
-<div class="d1"><img src="合作证明.jpg" width="100%" height="100%"/></div>
-<div style="width:600px;height:600px"></div>
+<center>
+    <form action="/upload" method="post"
+          enctype="multipart/form-data">
+
+        <input type="text" name="contentJson"/><br/>
+
+        <input type="file" name="file"/><br/>
+        <input type="file"
+               name="file"/><br/> <input type="file" name="file"/><br/> <input
+            type="submit" value="上 传"/>
+    </form>
+    <h5>上传结果：</h5>
+
+</center>
 </body>
 </html>
